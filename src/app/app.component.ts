0eeead89c3e6ba2788adfccdb00fc9cd037c3041
Aagam Doshi <aagamdoshi30794@gmail.com>
2019-06-30 22:20:33 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { StockService } from './stock.service';
+import { uniqBy, union, filter, find } from 'lodash';
+import { NbSearchService } from '@nebular/theme';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'StocksApp';
+export class AppComponent implements OnInit {
+  title = 'StocksApp | Lighting fast updates';
+  arrayData: {};
+  value = '';
+  constructor(private stockService: StockService,
+    private searchService: NbSearchService) {
+      this.searchService.onSearchSubmit()
+      .subscribe((data: any) => {
+        this.value = data.term;
+      })
+     }
+
+  ngOnInit() {
+    this.stockService.sendData.subscribe((value: {}) => {
+      let filledStatus = this.computeStock(this.arrayData, value);
+      this.arrayData = (uniqBy(union(filledStatus, this.arrayData), 'name'));
+    });
+  }
+
+  public computeStock(previous, current) {
+    let c = filter(current, function (o) {
+      find(previous, function (p) {
+        if (p.name == o.name) {
+          if (p.price > o.price) {
+            o.status = "danger";
+            o.diff = p.price - o.price;
+          }
+          else if (p.price < o.price) {
+            o.status = "success";
+            o.diff = o.price - p.price;
+          }
+          return o;
+        }
+      });
+      return current;
+    });
+    return c;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbCardModule, NbButtonModule, NbTooltipModule, NbSearchModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbCardModule,
+    NbButtonModule,
+    NbTooltipModule,
+    NbSearchModule
   ],
   providers: [],
   bootstrap: [AppComponent]
